e7b06630a5e212a0aa3117b66e5fbe276e956d80
Álvaro García Fuentes <gf_alvaro@hotmail.com>
2026-07-28 20:48:27 +0200

    creadas URLs de la página

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { Inicio } from './inicio/inicio';
 import { SesionForm } from './sesion-form/sesion-form';
+import { SobreNosotros } from './sobre-nosotros/sobre-nosotros';
+import { Productos } from './productos/productos';
+import { Contacto } from './contacto/contacto';
 
 export const routes: Routes = [
   { path: '', component: Inicio },
-  { path: 'sesion-form', component: SesionForm }
+  { path: 'sesion-form', component: SesionForm },
+  { path: 'sobre-nosotros', component: SobreNosotros },
+  { path: 'productos', component: Productos },
+  { path: 'contacto', component: Contacto }
 ];
