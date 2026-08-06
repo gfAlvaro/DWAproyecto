618ef38c9733f21e77d602661f4473781d8240f1
Álvaro García Fuentes <gf_alvaro@hotmail.com>
2026-08-07 00:45:56 +0200

    creado formulario de contacto

--- src/app/contacto/contacto.ts
+++ src/app/contacto/contacto.ts
@@ -1,11 +1,62 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  Validators,
+  ReactiveFormsModule,
+  FormGroup
+} from '@angular/forms';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from '../app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient()
+  ]
+};
+
 
 @Component({
-  selector: 'app-contacto',
-  imports: [],
-  templateUrl: './contacto.html',
-  styleUrl: './contacto.scss',
+  selector: 'app-contact',
+  imports: [ReactiveFormsModule],
+  templateUrl: './contacto.html'
 })
+
 export class Contacto {
 
+  contactForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient
+  ) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+  send() {
+    if (this.contactForm.invalid) {
+      this.contactForm.markAllAsTouched();
+      return;
+    }
+
+    this.http.post(
+      'https://tu-api.com/contacto',
+      this.contactForm.value
+    ).subscribe({
+      next: () => {
+        alert('Mensaje enviado');
+        this.contactForm.reset();
+      },
+      error: () => {
+        alert('Ha ocurrido un error');
+      }
+    });
+  }
 }
