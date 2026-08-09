40b3c581e224a4473cdd0a57102f7234e4972ed9
Álvaro García Fuentes <gf_alvaro@hotmail.com>
2026-08-09 02:59:17 +0200

    conexión api con frontend

--- api/server.js
+++ api/server.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/api/hola', (req, res) => {
+    res.json({
+        mensaje: 'Hola desde mi API'
+    });
+});
+
+app.post('/api/contacto', (req, res) => {
+  const { nombre, email, asunto, mensaje } = req.body;
+
+  console.log('Nuevo mensaje de contacto:');
+  console.log('Nombre:', nombre);
+  console.log('Email:', email);
+  console.log('Asunto:', asunto);
+  console.log('Mensaje:', mensaje);
+
   res.json({
-    mensaje: 'Hola desde mi API'
+    ok: true,
+    mensaje: 'Mensaje recibido correctamente'
   });
 });
 
-app.listen(3000, () => {
-  console.log('API funcionando en el puerto 3000');
+app.listen(PORT, () => {
+  console.log(`API funcionando en puerto ${PORT}`);
 });
